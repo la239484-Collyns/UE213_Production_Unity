36fcdee7551226338c3f145ae77ebf10876aa2cf
Alexein-Soyka <153502122+Alexein-Soyka@users.noreply.github.com>
2025-04-18 15:32:56 +0200

    MenuPause

--- Assets/Code/Scripts/SelectStage.cs
+++ Assets/Code/Scripts/SelectStage.cs
@@ -29,4 +29,35 @@ public class StartStage : MonoBehaviour
     {
         Application.Quit();
     }
+    public void RetryLevel(string sceneName)
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        SceneManager.LoadScene(sceneName);
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        AudioListener.pause = true;
+    }
+    public void Resume()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+
+    }
+    public void MainMenu()
+    {
+        Time.timeScale = 1;
+        AudioListener.pause = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 }
